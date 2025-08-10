54daa710b8e9562a497e7b1725011226543aca5d
Reuvin <reuvinhernandez22@gmail.com>
2025-08-10 19:29:12 +0800

    Update

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -19,7 +19,7 @@ function Navigations({ darkMode, darkModeChange }) {
   return (
     <nav
       className={`sticky top-0 z-50 p-4 flex items-center justify-between transition-colors ${
-        darkMode ? "text-white" : "bg-white text-black"
+        darkMode ? "text-white" : "text-black"
       }`}
     >
       <div className="flex items-center gap-2 lg:hidden">
@@ -29,21 +29,34 @@ function Navigations({ darkMode, darkModeChange }) {
             darkMode ? "hover:bg-gray-700" : "hover:bg-gray-200"
           }`}
         >
-          <FontAwesomeIcon
-            icon={darkMode ? faSun : faMoon}
-            className="text-xl"
-          />
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} className="text-xl" />
         </button>
       </div>
 
-    <div className="hidden lg:block xl:block relative flex-1 -mr-24">
+      <div className="hidden lg:block xl:block relative flex-1 -mr-24">
         <ul className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-8 text-lg">
-            <li><a href="#home" className="nav-underline">Home</a></li>
-            <li><a href="#about" className="nav-underline">About</a></li>
-            <li><a href="#projects" className="nav-underline">Project</a></li>
-            <li><a href="#contact" className="nav-underline">Contact Me</a></li>
+          <li>
+            <a href="#home" className="nav-underline">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-underline">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="nav-underline">
+              Project
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-underline">
+              Contact Me
+            </a>
+          </li>
         </ul>
-    </div>
+      </div>
 
       <div
         className="lg:hidden flex flex-col justify-center items-center w-8 h-8 cursor-pointer relative"
@@ -73,22 +86,27 @@ function Navigations({ darkMode, darkModeChange }) {
             darkMode ? "hover:bg-gray-700" : "hover:bg-gray-200"
           }`}
         >
-          <FontAwesomeIcon
-            icon={darkMode ? faSun : faMoon}
-            className="text-xl"
-          />
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} className="text-xl" />
         </button>
       </div>
 
       <div
-        className={`absolute top-16 right-0 w-48 bg-white dark:bg-slate-900 shadow-lg rounded-lg p-4 flex flex-col gap-4 transform transition-transform duration-300 lg:hidden ${
-          menuOpen ? "translate-x-0" : "translate-x-full"
-        }`}
+        className={`absolute top-16 right-0 w-48 shadow-lg rounded-lg p-4 flex flex-col gap-4 transform transition-transform duration-300 lg:hidden ${
+          darkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+        } ${menuOpen ? "translate-x-0" : "translate-x-full"}`}
       >
-        <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
-        <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
-        <a href="#projects" onClick={() => setMenuOpen(false)}>Project</a>
-        <a href="#contact" onClick={() => setMenuOpen(false)}>Contact Me</a>
+        <a href="#home" onClick={() => setMenuOpen(false)}>
+          Home
+        </a>
+        <a href="#about" onClick={() => setMenuOpen(false)}>
+          About
+        </a>
+        <a href="#projects" onClick={() => setMenuOpen(false)}>
+          Project
+        </a>
+        <a href="#contact" onClick={() => setMenuOpen(false)}>
+          Contact Me
+        </a>
       </div>
     </nav>
   );
